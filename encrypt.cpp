9af24482cd4757c4904c11e60373f15f079938a5
Luis J Pacheco <89895893+fredabomb50@users.noreply.github.com>
2023-06-17 09:09:42 -0400

    Update encrypt.cpp
    finished creating proxy message for shifting input values

--- encrypt.cpp
+++ encrypt.cpp
@@ -9,20 +9,47 @@ using namespace std;
 #define ARRAY_SIZE(arr) ( sizeof(arr) / sizeof(arr[0]) ) 
 
 
-std::string generate_token()
+std::string generate_token(int optional_seed)
 {
-    srand(time(NULL));
+    if(!optional_seed)
+    {
+        srand(optional_seed);
+    }
+    else
+    {
+        srand(time(NULL));
+    }
+    
     return std::to_string(rand() % 100000); // generate 5-digit random number
 }
 
 
+char generate_proxy_val(int seed)
+{
+    char result = '0';
+    
+    // generate by seed, a digit between 1 and 9
+    srand(time(NULL));
+    int rando = rand() % 10;
+    if(!rando && rando != 9) {rando++;}
+
+    // char '0' = 48 in ASCII.
+    // 48+1 = 49 in ASCII == '1' 
+    return result + rando;
+}
+//3161517655311334589161231771536838469
+
 int main ()
 {
+    // init
     int resultCode = 0;
     ofstream  test_output;
     test_output.open("token.txt");
 
+    // Create token to obfuscate encrypted message later
+    std::string token = generate_token(0);
 
+    // Get input message and populate buffer
     std::string input = "This is a big ass, motherfucking, test message.";
     std::string buffer = "";
     for(char c : input)
@@ -30,10 +57,21 @@ int main ()
         if(c != ' ' && c != ',' && c != '.') { buffer.append(1u, c); }
     }
 
-    test_output << buffer;
+    // Create proxy and new random for this session
+    srand(time(NULL));
+    int seed_increment = rand() % 1000;
+    std::string proxy = "";
 
-    // Create token to obfuscate encrypted message
-    std::string token = generate_token();
+    for(char c : buffer)
+    {
+        char temp = generate_proxy_val(seed_increment);
+        test_output << temp;
+        proxy.append(1u, temp);
+        seed_increment++;
+    }
+
+    //test_output << input << "\n" << buffer << "\n" << proxy;
+
     
     
     // close out file handles and exit
